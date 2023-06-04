9bb9c4d011c97193563963f6e5aa8249b53e13e1
Billy <2452372606@qq.com>
2023-06-04 14:33:23 +0800

    chore(views): ant Design of React的主题定制

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/index.css'
+import { ConfigProvider } from 'antd'
 import { StyleProvider } from '@ant-design/cssinjs'
 import App from './App'
-
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <StyleProvider hashPriority="high">
-    <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#955ce6',
+          colorSuccess: '#955ce6',
+          colorWarning: '#F5222D',
+          colorError: '#F5222D'
+        }
+      }}
+    >
       <App />
-    </React.StrictMode>
+    </ConfigProvider>
   </StyleProvider>
 )
 
